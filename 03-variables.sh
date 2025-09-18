e2930651019de3b75b0b0bb57c660747c38801c1
keerthana-ai123 <keerthanadevathu123@gmail.com>
2025-09-18 16:43:32 +0530

    some message

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # No space before or after equal
-PERSON1=john
-PERSON2=raheem
+PERSON1=sathish
+PERSON2=keerthana
 
 echo "$PERSON1:: Hi $PERSON2, How are you?"
 echo "$PERSON2:: Hello $PERSON1, I am fine. How are you?"
