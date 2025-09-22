6b4d4e4a3774a8f643044fbf8304bdff0fb0499e
keerthana-ai123 <keerthanadevathu123@gmail.com>
2025-09-22 16:59:02 +0530

    session-6

---
+++ 15-colors.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
+fi
+
+VALIDATE(){ # functions receive inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then   
+      echo -e "Installing $2 ... $R FAILURE $N"
+      exit 1
+else 
+    echo -e "Installing $2 ... $G SUCCESS $N"
+fi
+}
+
+dnf list installed mysql
+#Insatll if it is not found
+if [ $? -ne 0]; then
+    dnf install mysql -y
+    VALIDATE $? "MySQL"
+else
+    echo -e "MySQL already exist ... $Y SKIPPING $N"
+fi
+
+
+dnf list installed nginx
+if [ $? -ne 0]; then
+    dnf install nginx -y
+    VALIDATE $? "Nginx"
+else
+    echo -e "Nginx already exist ... $Y SKIPPING $N"
+fi
+
+dnf list installed python3
+if [ $? -ne 0]; then
+    dnf install python3 -y
+    VALIDATE $? "python3"
+lse
+    echo -e "python3 already exist ... $Y SKIPPING $N"
+fi
