cd76ecb0c700de36502865d31a210e606d0700d3
keerthana-ai123 <keerthanadevathu123@gmail.com>
2025-09-26 15:52:10 +0530

    variables

--- 18-loops.sh
+++ 18-loops.sh
@@ -7,12 +7,13 @@ N="\e[0m"
 
 LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 
 USERID=$(id -u)
 
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+
 if [ $USERID -ne 0 ]; then
     echo "ERROR:: Please run this script with root privelege"
     exit 1 # failure is other than 0
