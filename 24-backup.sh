94b9f0815f993b1283900c20287f90cbcfeb3113
keerthana-ai123 <keerthanadevathu123@gmail.com>
2025-09-26 18:21:24 +0530

    variables

--- 24-backup.sh
+++ 24-backup.sh
@@ -30,20 +30,25 @@ USAGE(){
     exit 1
 }
 
+### check source dir and dest dir pass or not
 if [ $# -lt 2 ]; then
     USAGE
 fi
 
+## source dir exists or not
 if [ ! -d "$SOURCE_DIR" ]; then
    echo -e "$R Source $SOURCE_DIR does not exist $N"
    exit 1
 fi
 
+## DEST dir exists or not
 if [ ! -d "$DEST_DIR" ]; then
    echo -e "$R Destination $DEST_DIR does not exist $N"
    exit 1
 fi
 
+
+## FIND the Files
 FILES=$(find "$SOURCE_DIR" -name "*.log" -type f -mtime +"$DAYS")
 
 if [ -n "$FILES" ]; then
@@ -52,9 +57,24 @@ if [ -n "$FILES" ]; then
     ZIP_FILE_NAME="$DEST_DIR/app-logs-$TIMESTAMP.zip"
     zip -r "$ZIP_FILE_NAME" $FILES
     echo "Archived to $ZIP_FILE_NAME"
+ 
+ ## Check archieve success
+  if [ -f $ZIP_FILE_NAME ]
+then
+echo -e "Archieve .... $G SUCCESS $N"
+
+while IFS= read -r filepath
+do
+  echo "Deleting the file: $filepath"
+  rm -rf $filepath
+  echo "Deleted the file: $filepath"
+done <<< $FILES_TO_DELETE
+
 else
-    echo -e "No files to archive.... $Y SKIPPING $N"
-    exit 0
+    echo -e "Archieve .... $R FAILURE $N"
+    exit 1
+else
+  echo -e "No Files to archieve .... $Y SKIPPING $N"
 fi
 
 
