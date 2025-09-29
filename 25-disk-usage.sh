dd0aa005bbf201a0182e2efe53b7397b9f01f02f
keerthana-ai123 <keerthanadevathu123@gmail.com>
2025-09-29 15:56:01 +0530

    variables

--- 25-disk-usage.sh
+++ 25-disk-usage.sh
@@ -2,14 +2,18 @@
 
 DISK_USAGE=$(df -hT | grep -v Filesystem)
 DISK_THRESHOLD=2 # in project we keep it as 75
+IP_ADDRESS=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+MESSAGE=""
 
 while IFS= read -r line
 do
    USAGE=$(echo $line | awk '{print $6}' |  cut -d "%" -f1)
    PARTITION=$(echo $line | awk '{print $7}')
    if [ $USAGE -ge $DISK_THRESHOLD ]; then
-        echo "High usage on $PARTITION: $USAGE"
+        MESSAGE="High Disk usage on $PARTITION: $USAGE %"
     fi
 
 done <<< $DISK_USAGE
+
+echo "Message Body: $MESSAGE"
 
